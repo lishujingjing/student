cf07d6a8bf84e47ca1dbbf3c9e4f7668907885db
lishu <2676065740@qq.com>
2020-09-07 17:02:37 +0800

    在家继续开发的签到功能

---
+++ SignIn.java
@@ -0,0 +1,11 @@
+public Class SignIn{
+	private int id;
+	private String age;
+
+	public void sign(){
+		system.out.println("在家开发的代码");
+		}
+	public void sign (int id){
+		System.out.println("在家开发的代码")
+		}
+	}
